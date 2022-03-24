9cf494f7031ba170c616cd40211724ebbe9ecf13
zhanglei <1307995780@qq.com>
2022-03-24 19:47:35 +0800

    前端除分支 已完成

--- assets/js/art_cate/art_list.js
+++ assets/js/art_cate/art_list.js
@@ -5,7 +5,7 @@ $(function() {
         cate_id: '', // 文章分类的 id
         state: '' // 文章的发布状态
     };
-
+    let laypage = layui.laypage;
     let data = {
         stauts: 0,
         msg: '获取成功',
@@ -31,6 +31,8 @@ $(function() {
             data: query,
             success: function(response) {
                 if (response.status != 0) return false
+                initCates();
+                renderPage()
             }
         });
     }
@@ -41,7 +43,10 @@ $(function() {
             success: function(response) {
                 if (response.status != 0) return false
                 let dl = $('div.layui-unselect').eq(0).find('.layui-anim');
-                dl.html(template('tpl-cate', response));
+                let html = template('tpl-cate', response);
+                $('[name=cate_id]').html(html)
+                layui.form.render()
+                    // dl.html(template('tpl-cate', response));
             }
         });
     }
@@ -60,12 +65,39 @@ $(function() {
         return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss;
     }
 
-    initCates();
     // layui不能动态生成添加的dom元素, 下列是为生成的dd添加事件,使input显示选择的文本
-    $('.layui-inline').eq(0).on('click', 'dd', function(even) {
-        $(this).parents('.layui-inline').find('.layui-input').val($(this).html()); // 不能用箭头函数,因为箭头指向window
-    })
+    // $('.layui-inline').eq(0).on('click', 'dd', function(even) {
+    //     console.log(132);
+    //     $(this).parents('.layui-inline').find('.layui-input').val($(this).html()); // 不能用箭头函数,因为箭头指向window
+    // })
 
-
+    // 自定义的假数据
     $('tbody').html(template('tpl-table', data));
+
+
+    $('#form-search').on('submit', function(even) {
+        even.preventDefault();
+        // 获取表单选择的值
+        query.cate_id = $('[name=cate_id]').val();
+        query.state = $('[name=state]').val();
+        console.log(query);
+        // $.ajax({
+        //     type: 'post',
+        //     ulr: ''
+        // })
+    });
+
+    // 渲染分页方法
+    const renderPage = function(total) {
+        console.log(laypage);
+        laypage.render({
+            elem: 'pageBox',
+            count: total, //总数据调试
+            limit: query.pagesize, // 一页显示几条数据
+            curr: query.pagenum // 默认被选择的分页
+        });
+    }
+
+    initCates();
+    renderPage(7);
 })
